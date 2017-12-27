74611958d53c2322bff9ee5a9559095a4d954dc1
Kevin Rousseeuw <k.rousseeuw@hotmail.com>
2017-12-27 17:30:37 +0100

    Les5 afgewerkt

--- les5/src/voorbeeld/MijnButtonListener.java
+++ les5/src/voorbeeld/MijnButtonListener.java
@@ -1,5 +1,12 @@
 package voorbeeld;
 
-public class MijnButtonListener {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+public class MijnButtonListener implements ActionListener {
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		System.out.println("De knop werd aangeklikt.");
+	}	
 }
