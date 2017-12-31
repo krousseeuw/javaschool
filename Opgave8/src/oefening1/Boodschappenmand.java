3896befa4208cb30e15d19bc163e0ef94a7b7ea1
Kevin Rousseeuw <k.rousseeuw@hotmail.com>
2017-12-31 15:35:43 +0100

    Start opgave 7 of 1

---
+++ Opgave8/src/oefening1/Boodschappenmand.java
@@ -0,0 +1,12 @@
+package oefening1;
+
+import java.io.Serializable;
+
+public class Boodschappenmand implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1383625323009533651L;
+
+}
